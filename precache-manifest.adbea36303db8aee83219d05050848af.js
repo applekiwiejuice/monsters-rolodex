9d418324859301da7a3352aba59ec0f3117e7ae6
Mcnikko Pinlac <mcnikkopinlacnetflix@gmail.com>
2020-10-02 23:19:01 +0800

    Updates

---
+++ precache-manifest.adbea36303db8aee83219d05050848af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5b8cfdbaba7f1d0c56d8e2ddbd14b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a52470560f07b2ebe525",
+    "url": "/monsters-rolodex/static/css/main.65b56e02.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52470560f07b2ebe525",
+    "url": "/monsters-rolodex/static/js/main.e2d35356.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
